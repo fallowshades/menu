f04244b6f8ab290b1e0f8d49f7520f55b66ed93d
Erik Jonsson <42693213+fallowshades@users.noreply.github.com>
2024-10-24 10:03:00 +0200

    prep ts config

---
+++ README.md
@@ -0,0 +1,11 @@
+```sh
+npx parcel index.html
+```
+
+- problemmatic
+
+```sh
+rm -rf .parcel-cache
+rm -rf node_modules
+rm package-lock.json
+```
--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -142,14 +142,14 @@
       this[globalName] = mainExports;
     }
   }
-})({"7EhiY":[function(require,module,exports) {
+})({"5M0Ek":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
-module.bundle.HMR_BUNDLE_ID = "0907ca6d3464ddca";
+module.bundle.HMR_BUNDLE_ID = "fe4256060641b553";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -583,8 +583,183 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
     });
 }
 
-},{}],"j4kuM":[function(require,module,exports) {
+},{}],"bNKaB":[function(require,module,exports) {
+var _data = require("./data");
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function() {
+    diplayMenuItems((0, _data.menu));
+    displayMenuButtons();
+});
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function(item) {
+        // console.log(item);
+        return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+              
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+    const categories = (0, _data.menu).reduce(function(values, item) {
+        if (!values.includes(item.category)) values.push(item.category);
+        return values;
+    }, [
+        "all"
+    ]);
+    const categoryBtns = categories.map(function(category) {
+        return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    }).join("");
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+    filterBtns.forEach(function(btn) {
+        btn.addEventListener("click", function(e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = (0, _data.menu).filter(function(menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) return menuItem;
+            });
+            if (category === "all") diplayMenuItems((0, _data.menu));
+            else diplayMenuItems(menuCategory);
+        });
+    });
+}
 
-},{}]},["7EhiY","j4kuM"], "j4kuM", "parcelRequirea8e2")
+},{"./data":"9kapS"}],"9kapS":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "menu", ()=>menu);
+const menu = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `
+    },
+    {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: 13.99,
+        img: "./images/item-2.jpeg",
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `
+    },
+    {
+        id: 3,
+        title: "godzilla milkshake",
+        category: "shakes",
+        price: 6.99,
+        img: "./images/item-3.jpeg",
+        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
+    },
+    {
+        id: 4,
+        title: "country delight",
+        category: "breakfast",
+        price: 20.99,
+        img: "./images/item-4.jpeg",
+        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `
+    },
+    {
+        id: 5,
+        title: "egg attack",
+        category: "lunch",
+        price: 22.99,
+        img: "./images/item-5.jpeg",
+        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `
+    },
+    {
+        id: 6,
+        title: "oreo dream",
+        category: "shakes",
+        price: 18.99,
+        img: "./images/item-6.jpeg",
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`
+    },
+    {
+        id: 7,
+        title: "bacon overflow",
+        category: "breakfast",
+        price: 8.99,
+        img: "./images/item-7.jpeg",
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `
+    },
+    {
+        id: 8,
+        title: "american classic",
+        category: "lunch",
+        price: 12.99,
+        img: "./images/item-8.jpeg",
+        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
+    },
+    {
+        id: 9,
+        title: "quarantine buddy",
+        category: "shakes",
+        price: 16.99,
+        img: "./images/item-9.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
+    },
+    {
+        id: 10,
+        title: "bison steak",
+        category: "dinner",
+        price: 22.99,
+        img: "./images/item-10.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
+    }
+];
 
-//# sourceMappingURL=erik-filmon.3464ddca.js.map
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"8YDnT"}],"8YDnT":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || Object.prototype.hasOwnProperty.call(dest, key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
+
+},{}]},["5M0Ek","bNKaB"], "bNKaB", "parcelRequirea8e2")
+
+//# sourceMappingURL=index.0641b553.js.map
